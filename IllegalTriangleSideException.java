e6a4b026bb4dab7e32cdf2e846488bf477bd9e45
ManuAchar <106185012+MAchar1@users.noreply.github.com>
2022-10-17 01:54:32 -0400

    Create IllegalTriangleSideException.java

---
+++ IllegalTriangleSideException.java
@@ -0,0 +1,9 @@
+public class IllegalTriangleSideException extends Exception{
+    public IllegalTriangleSideException(){
+        super();
+    }
+
+    public IllegalTriangleSideException(String message){
+        super(message);
+    }
+}
