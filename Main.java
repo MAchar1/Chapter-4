02b813ce3a028d7f8218cda03f8bd344326fedce
ManuAchar <106185012+MAchar1@users.noreply.github.com>
2022-10-17 01:55:11 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,26 @@
+/**
+ * name: Manu Achar
+ * date: 10/16/22
+ * class: Main
+ * file: Main.java
+ * description: This is the check for the exercise 3. This program creates and then checks 3 triangles.
+ */
+
+public class Main {
+    public static void main(String[] args) throws IllegalTriangleSideException {
+
+        Triangle tri1 = new Triangle(3, 4, 5);
+        Triangle tri2 = new Triangle(10, 20, 20);
+        Triangle tri3 = new Triangle(1, 5, 1);
+
+        try {
+            tri1.checkSides();
+            tri2.checkSides();
+            tri3.checkSides();
+        } catch (IllegalTriangleSideException e) {
+            System.out.println("This is not a triangle");
+        }
+
+
+    }
+}
